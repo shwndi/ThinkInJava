66bbb4af6b948d1714e3764954c29b3d2f7f7968
chenzy <chenzhangyi@zrar.com>
2020-07-31 20:32:32 +0800

    java 编程思想测试10章

---
+++ src/com/company/chapterten/AnonymousConstructor.java
@@ -0,0 +1,38 @@
+package com.company.chapterten;
+
+/**
+ * 多字段初始化时,匿名内部类由于没有名称，所以不能使用构造器
+ * 但可以通过初始化达到构造器的效果。
+ * 通过new 对象传入到基类之中，不会被匿名了内部类直接使用。
+ *
+ * @author czy
+ * @date 2020-7-31
+ */
+public class AnonymousConstructor {
+	public static Base getBase(int i) {
+		return new Base(i) {
+			{
+				System.out.println("Inside instance initialize");
+			}
+
+			@Override
+			public void f() {
+				System.out.println("In anonymous f()");
+			}
+		};
+	}
+
+	public static void main(String[] args) {
+		Base base = getBase(47);
+		base.f();
+	}
+
+}
+
+abstract class Base {
+	public Base(int i) {
+		System.out.println("Base.Constructor.i=" + i);
+	}
+
+	public abstract void f();
+}
--- src/com/company/chapterten/Pracel9.java
+++ src/com/company/chapterten/Pracel9.java
@@ -2,6 +2,8 @@ package com.company.chapterten;
 
 /**
  * 匿名内部类可以对字段进行初始化
+ * 适合于单字段
+ * 通过final然后作用范围辐射到内部类中使用。
  *
  * @author czy
  * @date 2020-7-30
