b05ac5eddd06edd9048c0e42f606517f1462ff6f
陈张一 <chenzhangyi@zrar.com>
2021-01-20 14:26:16 +0800

    add：添加工具包

---
+++ src/dateType/test3.java
@@ -0,0 +1,28 @@
+package dateType;
+
+import cn.hutool.json.JSONUtil;
+
+/**
+ * @author czy
+ * @date 2021-1-20
+ */
+public class test3 {
+
+	public static void main(String[] args) {
+	class A{
+		String a;
+
+		@Override
+		public String toString() {
+			return "A{" +
+					"a='" + a + '\'' +
+					'}';
+		}
+	}
+		A a = new A();
+		a.a="123";
+		System.out.println(a);
+		String s = JSONUtil.toJsonStr(a.toString());
+		System.out.println(s);
+	}
+}
